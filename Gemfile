e1f3e1d99b36f7b5b5de53e959a4595a646bdb99
John <john.romas87@gmail.com>
2014-10-08 23:31:12 +0000

    Added Devise

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'pdfkit'
 
 gem 'wkhtmltopdf-binary'
 
+gem 'devise'
+
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Gcs::Application.routes.draw do
+  devise_for :users
+
   get "dashboard" => "dashboard#index", as: "dashboard"
+  
   resources :attorneys
 
   resources :documents
@@ -19,7 +22,7 @@ Gcs::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'claims#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
