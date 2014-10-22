3f4a9ea4240a115942f124308ba30ebee70e0252
John <john.romas87@gmail.com>
2014-10-22 19:43:53 +0000

    Modified production.rb to include email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,17 @@ Gcs::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { host: 'gcs.globalclaimssolutionsllc.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "globalclaimssolutionsllc.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV['GMAIL_USER'],
+    password: ENV['GMAIL_PASS']
+  }
 end
